a0a8595c60bf0c24d60c8c63ec65178291dca15d
mehrshad <microsoftmehr@gmail.com>
2023-01-26 11:58:23 +0330

    heloo

--- first.js
+++ first.js
@@ -1,2 +1,12 @@
-var x = 23 ;
-console.log(x);
+// To Upper Function
+function ToUpper(username){
+    let strUsername = String(username);
+    console.log(strUsername.toUpperCase());
+}
+ToUpper('mehrshad');
+
+// inches to cm 
+function inchesToCm(lenght){
+    console.log(lenght * 2.54 );
+}
+inchesToCm(2.5);
